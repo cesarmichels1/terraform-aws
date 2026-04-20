e12c3dac35f0bfd4ca026ec104cfab4287f0e798
Cesar Michels <cesarmichels1@gmail.com>
2026-04-20 20:13:28 -0300

    remote backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,11 @@ terraform {
       version = "6.41.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform-aws-385109575918-us-east-1-an"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
